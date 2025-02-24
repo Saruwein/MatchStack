28fc043286a23ce788479ebbe7caf6925a6300ec
adam-h8dn0orandrq32x <franois@lfi.rwth-aachen.de>
2025-02-24 19:02:12 +0100

    Pray for .gitignore to do its job + Icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.matchstack"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.matchstack"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
